df128044fa584f4e37f1f3bbe4108b0cdefd006c
Lucas Pinheiro <165809855+Pinheiro-Lucasz@users.noreply.github.com>
2024-11-17 16:51:28 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int cpf = 241296;
+        System.out.println(cpf);
+
+        Integer cpf1 = cpf;
+        System.out.println(cpf1);
+    }
+}
